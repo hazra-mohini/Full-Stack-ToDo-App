4b1256a17ef48adc6123ed5cce429b953b20cf59
hazra-mohini <hazramohini48@gmail.com>
2024-06-09 23:04:00 +0530

    Admin

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const routes = require('./routes/ToDoRoute')
+
+require('dotenv').config()
+
+const app = express()
+const PORT = process.env.port || 8000
+
+app.use(express.json())
+app.use(cors())
+
+const MONGOURL = process.env.MONGO_URL;
+
+mongoose.connect(MONGOURL).then(() => console.log(`Connected to MongoDB...`)).catch((err)=> console.log(err))
+ app.use(routes)
+app.listen(PORT, ()=> console.log(`Listening on: ${PORT}`))
